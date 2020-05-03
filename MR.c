1c2bb52564090c6873c50d7aef2503dec6758a27
monta0315 <63449578+monta0315@users.noreply.github.com>
2020-05-03 14:34:21 +0900

    5/3 コンテスト回答
    5/2のコンテストのC問題
    全探索に必要な配列の作成方法を学べる

--- MR.c
+++ MR.c
@@ -1,20 +1,36 @@
 #include<stdio.h>
-int main(){
-  int N,M,Q;
-  scanf("%d%d%d",&N,&M,&Q);
-  int i;
-  int a[51],b[51],c[51],d[51];
-  for(i=0;i<Q;i++){
-    scanf("%d\n",&a[i]);
-    scanf("%d\n",&b[i]);
-    scanf("%d\n",&c[i]);
-    scanf("%d\n",&d[i]);
+  int N,M,Q,max=0;
+  int a[55],b[55],c[55],d[55];
+  int A[15];
+  int check(){
+    int ans=0;
+    for(int i=1;i<=Q;i++){
+      if(A[b[i]]-A[a[i]]==c[i]){
+        ans+=d[i];
+      }
+    }
+    return ans;
+  }//問題文の条件をチェックする、これをするためにA[i]を別のところで作成する必要がある
+  void make(int i,int t){
+    if(i==N+1){
+      int x=check();//作成されたA[i]が長さNを満たした時関数checkに移行する
+      if(x>max){
+        max=x;
+      }//返ってきたd[i]の総和をmaxと比較
+      return ;
+    }
+    for(int j=t;j<=M;j++){//再帰でA[i]を作成している全探索の際に用いる？
+      A[i]=j;
+      make(i+1,j);
+    }
   }
-  for(i=0;i<Q;i++){
-    printf("%d\n",a[i]);
-    printf("%d\n",b[i]);
-    printf("%d\n",c[i]);
-    printf("%d\n",d[i]);
+  int main(){
+    scanf("%d%d%d",&N,&M,&Q);
+    int i=1;
+    for(i=1;i<=Q;i++){
+      scanf("%d%d%d%d",&a[i],&b[i],&c[i],&d[i]);
+    }
+    make(1,1);
+    printf("%d",max);
+    return 0;
   }
-  return 0;
-}
